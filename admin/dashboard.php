748663a5aa7f4a44915387517116b8ce1d9b8481
johnpaul <johnpaulsaspasia1702@gmail.com>
2024-11-20 15:03:58 +0800

    Done Setup Subject Directory!

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,4 +1,6 @@
 <?php
+require '../functions.php';
+
 $logoutPage = 'logout.php';
 require './partials/header.php';
 require './partials/side-bar.php';
---
+++ admin/subjects/edit.php
@@ -0,0 +1,12 @@
+<?php
+require '../functions.php';
+
+$logoutPage = 'logout.php';
+require './partials/header.php';
+require './partials/side-bar.php';
+?>
+
+<?php
+require './partials/footer.php';
+
+?>
