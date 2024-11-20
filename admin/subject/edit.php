e1709d2d613c5c87af6aef825f0d01b0c3e0c932
johnpaul <johnpaulsaspasia1702@gmail.com>
2024-11-21 00:48:02 +0800

    Done Creating Breadcrumb on edit!

--- admin/subject/edit.php
+++ admin/subject/edit.php
@@ -1,12 +1,25 @@
 <?php
-require '../functions.php';
+include '../../functions.php'; // Include the functions
+include '../partials/header.php';
 
-$logoutPage = 'logout.php';
-require './partials/header.php';
-require './partials/side-bar.php';
-?>
-
-<?php
-require './partials/footer.php';
+$logoutPage = '../logout.php';
+$dashboardPage = '../dashboard.php';
+$subjectPage = './add.php';
+include '../partials/side-bar.php';
 
 ?>
+<div class="col-md-9 col-lg-10">
+
+    <h3 class="text-left mb-5 mt-5">Edit Subject</h1>
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item" aria-current="page"><a href="../dashboard.php">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="add.php">Add Subject</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Edit Subject</li>
+            </ol>
+        </nav>
+        <?php
+        include '../partials/footer.php';
+
+        ?>
