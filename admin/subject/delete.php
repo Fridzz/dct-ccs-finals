0c5aa0da941d6a57b62131e3e4261dee4752dc15
johnpaul <johnpaulsaspasia1702@gmail.com>
2024-11-21 01:16:55 +0800

    Done Create delte breadcramp!

--- admin/subject/delete.php
+++ admin/subject/delete.php
@@ -1,12 +1,27 @@
 <?php
-require '../functions.php';
+include '../../functions.php'; // Include the functions
+include '../partials/header.php';
 
-$logoutPage = 'logout.php';
-require './partials/header.php';
-require './partials/side-bar.php';
+$logoutPage = '../logout.php';
+$dashboardPage = '../dashboard.php';
+
+$subjectPage = './add.php';
+include '../partials/side-bar.php';
+
+
 ?>
+<div class="col-md-9 col-lg-10">
 
-<?php
-require './partials/footer.php';
+    <h3 class="text-left mb-5 mt-5">Delete Subject</h3>
 
-?>
+    <!-- Breadcrumb Navigation -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="../dashboard.php">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="add.php">Add subject</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Delete Subject</li>
+        </ol>
+    </nav>
+    <?php
+    include '../partials/footer.php';
+    ?>
